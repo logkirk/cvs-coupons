3541ca9be05364a6e7c5411f577e74f53a9649ed
Logan Kirkland <43626270+logkirk@users.noreply.github.com>
2022-12-07 00:47:41 -0500

    Refactored into a class. Changed hardcoded user, pass, dob into terminal inputs. Added TODO for survey modal code I forgot to implement.

--- CVSCoupons.py
+++ CVSCoupons.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from time import sleep
+from getpass import getpass
 
 from selenium.common import NoSuchElementException
 from undetected_chromedriver import Chrome
@@ -9,9 +10,6 @@ from selenium.webdriver.support import expected_conditions as ec
 
 SLEEP_TIME = 1
 URL = "https://www.cvs.com/extracare/home"
-EMAIL = ""  # Redacted
-PASSWORD = ""  # Redacted
-DOB = ""  # Redacted
 
 
 class SlowChrome(Chrome):
@@ -24,126 +22,134 @@ class SlowChrome(Chrome):
         return super(SlowChrome, self).__getattribute__(item)
 
 
-def initialize_webdriver():
-    return SlowChrome()
+class CVSCouponGrabber:
+    def __init__(self):
+        self.email = input("Enter your email: ")
+        self.password = getpass("Enter your password: ")
+        self.date_of_birth = input("Enter your date of birth (MMDDYYYY): ")
+        self.driver = SlowChrome()
 
+    def main(self):
+        self.driver.get(URL)
 
-def do_automation(driver_):
-    driver_.get(URL)
+        # Click 'Sign in' button
+        self.wait_until_visible_by_locator((By.XPATH, "//a[contains(text(), 'Sign in')]")).click()
 
-    # Click 'Sign in' button
-    wait_until_visible_by_locator(driver_, (By.XPATH, "//a[contains(text(), 'Sign in')]")).click()
+        # Enter email
+        self.wait_until_visible_by_locator((By.XPATH, "//input[@id='emailField']")).send_keys(self.email)
+        self.email = None
 
-    # Enter email
-    wait_until_visible_by_locator(driver_, (By.XPATH, "//input[@id='emailField']")).send_keys(EMAIL)
+        # Continue to password entry
+        self.driver.find_element(By.XPATH, "//button[contains(@class, 'continue-button')]").click()
 
-    # Continue to password entry
-    driver_.find_element(By.XPATH, "//button[contains(@class, 'continue-button')]").click()
-
-    # Enter password
-    wait_until_visible_by_locator(driver_, (By.XPATH, "//input[@id='cvs-password-field-input']")).send_keys(PASSWORD)
-
-    # Click 'Sign in' button
-    driver_.find_element(By.XPATH, "//button[contains(text(), 'Sign in')]").click()
-
-    # Enter date of birth (content within shadow DOM)
-    shadow_root = wait_until_visible_by_locator(driver_, (By.XPATH, "//cvs-dob-validation-form")).shadow_root
-    wait_until_visible_by_locator(shadow_root, (By.NAME, "dob")).send_keys(DOB)
-    # shadow_root.find_element(By.NAME, "dob").send_keys(DOB)  # Can't seem to use XPATH within shadow DOM
-
-    # Click 'Continue' button
-    shadow_root.find_element(By.CLASS_NAME, "continueButton").click()
-
-    # Scroll to bottom to load all dynamic content
-    wait_until_visible_by_locator(driver_, (By.XPATH, "//cvs-coupon-container"))
-    scroll_to_bottom_of_dynamic_webpage(driver_)
-
-    # Dismiss survey modal if present
-    try:
-        driver_.find_element(By.XPATH, "TODO").click()
-    except NoSuchElementException:
-        pass
-
-    # Print coupon info
-    all_coupon_elems = driver_.find_elements(By.XPATH, "//cvs-coupon-container")
-    sent_coupon_elems = driver_.find_elements(By.XPATH, "//cvs-coupon-container[.//send-to-card-action/on-card]")
-    unsent_coupon_elems = driver_.find_elements(By.XPATH, "//cvs-coupon-container[.//send-to-card-action/button]")
-    print("Already on card: {}/{}".format(len(sent_coupon_elems), len(all_coupon_elems)))
-    print_coupons(sent_coupon_elems)
-    print("Not on card: {}/{}".format(len(unsent_coupon_elems), len(all_coupon_elems)))
-    print_coupons(unsent_coupon_elems)
-    print()
-
-    # Send all to card
-    send_coupons_to_card(unsent_coupon_elems)
-
-
-def wait_until_visible_by_locator(driver_, locator, timeout=10):
-    return WebDriverWait(driver_, timeout).until(ec.visibility_of_element_located(locator))
-
-
-def wait_until_present_by_locator(driver_, locator, timeout=10):
-    return WebDriverWait(driver_, timeout).until(ec.presence_of_element_located(locator))
-
-
-def scroll_to_bottom_of_dynamic_webpage(driver_, content_load_wait=0.1, timeout=30):
-    last_height = None
-    new_height = get_scroll_height(driver_)
-    start_time = datetime.now()
-    while new_height != last_height:
-        if (datetime.now() - start_time).total_seconds() > timeout:
-            raise TimeoutError("Timed out trying to scroll to bottom of dynamic webpage.")
-        scroll_to_bottom(driver_)
-        sleep(content_load_wait)
-        last_height = new_height
-        new_height = get_scroll_height(driver_)
-
-
-def get_scroll_height(driver_):
-    return driver_.execute_script("return document.body.scrollHeight")
-
-
-def scroll_to_bottom(driver_):
-    driver_.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-
-def print_coupons(coupon_elems):
-    for index, elem in enumerate(coupon_elems):
-        title = elem.find_element(By.XPATH, ".//*[contains(@class, 'coupon-title')]").text
-        sub_heading = elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-sub-heading')]").text
-        details = elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-details')]").text
-        exp_date = (
-            elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-exp-date')]")
-            .text.lower()
-            .lstrip("exp ")
-            .rstrip("mfr")
+        # Enter password
+        self.wait_until_visible_by_locator((By.XPATH, "//input[@id='cvs-password-field-input']")).send_keys(
+            self.password
         )
+        self.password = None
 
-        print(
-            "    {number}. {title}{sub_heading}\n"
-            "        Details: {details}\n"
-            "        Expires: {exp_date}".format(
-                number=index + 1,
-                title=title,
-                sub_heading=": " + sub_heading if sub_heading != "" else "",
-                details=details,
-                exp_date=exp_date,
+        # Click 'Sign in' button
+        self.driver.find_element(By.XPATH, "//button[contains(text(), 'Sign in')]").click()
+
+        # Enter date of birth (content within shadow DOM)
+        shadow_root = self.wait_until_visible_by_locator((By.XPATH, "//cvs-dob-validation-form")).shadow_root
+        self.wait_until_visible_by_locator((By.NAME, "dob"), driver=shadow_root).send_keys(self.date_of_birth)
+        self.date_of_birth = None
+
+        # Click 'Continue' button
+        shadow_root.find_element(By.CLASS_NAME, "continueButton").click()
+
+        # Scroll to bottom to load all dynamic content
+        self.wait_until_visible_by_locator((By.XPATH, "//cvs-coupon-container"))
+        self.scroll_to_bottom_of_dynamic_webpage()
+
+        # Dismiss survey modal if present
+        try:
+            self.driver.find_element(By.XPATH, "TODO").click()  # TODO
+        except NoSuchElementException:
+            pass
+
+        # Print coupon info
+        all_coupon_elems = self.driver.find_elements(By.XPATH, "//cvs-coupon-container")
+        sent_coupon_elems = self.driver.find_elements(
+            By.XPATH, "//cvs-coupon-container[.//send-to-card-action/on-card]"
+        )
+        unsent_coupon_elems = self.driver.find_elements(
+            By.XPATH, "//cvs-coupon-container[.//send-to-card-action/button]"
+        )
+        print("Already on card: {}/{}".format(len(sent_coupon_elems), len(all_coupon_elems)))
+        self.print_coupons(sent_coupon_elems)
+        print("Not on card: {}/{}".format(len(unsent_coupon_elems), len(all_coupon_elems)))
+        self.print_coupons(unsent_coupon_elems)
+        print()
+
+        # Send all to card
+        self.send_coupons_to_card(unsent_coupon_elems)
+
+    def wait_until_visible_by_locator(self, locator, driver=None, timeout=10):
+        if driver is None:
+            driver = self.driver
+        return WebDriverWait(driver, timeout).until(ec.visibility_of_element_located(locator))
+
+    def wait_until_present_by_locator(self, locator, driver=None, timeout=10):
+        if driver is None:
+            driver = self.driver
+        return WebDriverWait(driver, timeout).until(ec.presence_of_element_located(locator))
+
+    def scroll_to_bottom_of_dynamic_webpage(self, content_load_wait=0.1, timeout=30):
+        last_height = None
+        new_height = self.get_scroll_height()
+        start_time = datetime.now()
+        while new_height != last_height:
+            if (datetime.now() - start_time).total_seconds() > timeout:
+                raise TimeoutError("Timed out trying to scroll to bottom of dynamic webpage.")
+            self.scroll_to_bottom()
+            sleep(content_load_wait)
+            last_height = new_height
+            new_height = self.get_scroll_height()
+
+    def get_scroll_height(self):
+        return self.driver.execute_script("return document.body.scrollHeight")
+
+    def scroll_to_bottom(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def print_coupons(self, coupon_elems):
+        for index, elem in enumerate(coupon_elems):
+            title = elem.find_element(By.XPATH, ".//*[contains(@class, 'coupon-title')]").text
+            sub_heading = elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-sub-heading')]").text
+            details = elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-details')]").text
+            exp_date = (
+                elem.find_element(By.XPATH, ".//div[contains(@class, 'coupon-exp-date')]")
+                .text.lower()
+                .lstrip("exp ")
+                .rstrip("mfr")
             )
-        )
 
+            print(
+                "    {number}. {title}{sub_heading}\n"
+                "        Details: {details}\n"
+                "        Expires: {exp_date}".format(
+                    number=index + 1,
+                    title=title,
+                    sub_heading=": " + sub_heading if sub_heading != "" else "",
+                    details=details,
+                    exp_date=exp_date,
+                )
+            )
 
-def send_coupons_to_card(coupon_elems):
-    total_num = len(coupon_elems)
-    for index, elem in enumerate(coupon_elems):
-        print("Sending {}/{}...".format(index + 1, total_num))
-        elem.find_element(By.XPATH, ".//send-to-card-action/button").click()
-        wait_until_visible_by_locator(elem, (By.XPATH, ".//send-to-card-action/on-card"))
-    print("All coupons sent.")
+    def send_coupons_to_card(self, coupon_elems):
+        total_num = len(coupon_elems)
+        for index, elem in enumerate(coupon_elems):
+            print("Sending {}/{}...".format(index + 1, total_num))
+            elem.find_element(By.XPATH, ".//send-to-card-action/button").click()
+            self.wait_until_visible_by_locator((By.XPATH, ".//send-to-card-action/on-card"), driver=elem)
+        print("All coupons sent.")
 
 
 if __name__ == "__main__":
-    driver = initialize_webdriver()
+    grabber = CVSCouponGrabber()
     try:
-        do_automation(driver)
+        grabber.main()
     finally:
-        driver.close()
+        grabber.driver.close()
